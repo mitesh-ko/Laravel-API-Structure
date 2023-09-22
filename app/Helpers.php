bcc2e36fb8f17e187f44343b9d5595a08d6eaefa
Mitesh <mitesh>
2023-09-23 01:40:24 +0530

    New Helpers API response structure and exception.

---
+++ app/Helpers.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+
+if (!function_exists('apiResponse')) {
+    /**
+     * API response structure method.
+     *
+     * @param bool $status
+     * @param string $message
+     * @param mixed $data
+     * @param int $httpCode
+     * @return JsonResponse
+     */
+    function apiResponse(bool $status, string $message, mixed $data, int $httpCode = 200): JsonResponse
+    {
+        $response = [
+            'status' => $status,
+            'message' => $message,
+            'data'    => $data,
+        ];
+        return response()->json($response, $httpCode);
+    }
+}
+
+if (!function_exists('apiException')) {
+    /**
+     * API exception response method.
+     *
+     * @param $e
+     * @return JsonResponse
+     */
+    function apiException($e): JsonResponse
+    {
+        Log::debug("\nCode: " . $e->getCode() . "\nMessage: " . $e->getMessage() . "\nLine: " . $e->getLine());
+        return apiResponse(false, 'Something went wrong!', [], 500);
+    }
+}
