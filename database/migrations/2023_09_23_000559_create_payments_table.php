221320434a4714d10afeee34d5761f5f57d1cfa0
Mitesh <mitesh>
2023-09-23 16:23:47 +0530

    New migration added for Cart and Payment.

---
+++ database/migrations/2023_09_23_000559_create_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedInteger('payment_amount');
+            $table->unsignedTinyInteger('payment_status')->comment('0:Failed, 1:Success, 2:pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
