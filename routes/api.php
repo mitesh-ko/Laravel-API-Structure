70b5ab778384f9d41dffebf51900dc3f3cc4f795
Mitesh <mitesh>
2023-09-23 18:12:47 +0530

    Routes names added.

--- routes/api.php
+++ routes/api.php
@@ -14,16 +14,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('register', [GuestController::class, 'register']);
-Route::post('login', [GuestController::class, 'login']);
+Route::post('register', [GuestController::class, 'register'])->name('register');
+Route::post('login', [GuestController::class, 'login'])->name('login');
 
 Route::middleware('auth:api')->group(function () {
     Route::apiResource('products', ProductController::class)->except('show');
-    Route::post('add-to-cart', [CartController::class, 'addToCart']);
+    Route::post('add-to-cart', [CartController::class, 'addToCart'])->name('addToCart');
 
     Route::prefix('payment')->group(function () {
-        Route::get('initiate', [PaymentController::class, 'initiate']);
-        Route::get('success', [PaymentController::class, 'success']);
-        Route::get('failed', [PaymentController::class, 'failed']);
+        Route::get('initiate', [PaymentController::class, 'initiate'])->name('payment.initiate');
+        Route::get('success', [PaymentController::class, 'success'])->name('payment.success');
+        Route::get('failed', [PaymentController::class, 'failed'])->name('payment.failed');
     });
 });
